a607aa24df038cd0cb96f938b32e497e48dfedcb
SeetaRam <seetaramanjaneyulu172002@gmail.com>
2024-06-23 11:17:24 +0530

    Basic setup completed

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xs': '100px',
+      'sm': '350px',
+      'md': '640px',
+      'lg': '850px',
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+    },
+  },
+  plugins: [],
+}
+
