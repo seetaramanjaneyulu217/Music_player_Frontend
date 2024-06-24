51e2b6b9e2ff7c42d5b22c8724d91d6bf3c3431e
SeetaRam <seetaramanjaneyulu172002@gmail.com>
2024-06-24 16:50:11 +0530

    seeking song completed

--- src/sections/CurrentlyPlayingSongDisplay.tsx
+++ src/sections/CurrentlyPlayingSongDisplay.tsx
@@ -3,51 +3,65 @@ import { useDispatch, useSelector } from "react-redux";
 import { Song } from "../types";
 import options from "../assets/options.svg";
 import backward from "../assets/backward.svg";
-import play from '../assets/play.svg'
+import play from "../assets/play.svg";
 import pause from "../assets/pause.svg";
 import forward from "../assets/forward.svg";
 import sound from "../assets/sound.svg";
 import { passSongIndexToStore } from "../store/slices/song";
 
 const CurrentlyPlayingSongDisplay = () => {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const songRef = useRef<HTMLAudioElement>(null);
   const seekBar = useRef<HTMLHRElement>(null);
-  const songIndex = useSelector((state: any) => state.songs.songIndex)
+  const divRef = useRef<HTMLDivElement>(null)
+  const songIndex = useSelector((state: any) => state.songs.songIndex);
   const song: Song = useSelector((state: any) => state.songs.song);
-  const [currentSongAction, setCurrentSongAction] = useState<string>("play")
+  const [currentSongAction, setCurrentSongAction] = useState<"play" | "pause">(
+    "play"
+  );
 
   const changeColorOfTheSeeker = () => {
     if (songRef.current) {
-      return Math.floor((songRef.current.currentTime / songRef.current?.duration) * 100);
+      return Math.floor(
+        (songRef.current.currentTime / songRef.current.duration) * 100
+      );
+    }
+  };
+
+
+  const handleSeekBarClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (songRef.current && divRef.current) {
+      songRef.current.currentTime = (e.nativeEvent.offsetX / divRef.current.offsetWidth) * songRef.current.duration
     }
   };
 
   useEffect(() => {
-    let interval: NodeJS.Timeout
-    if(currentSongAction === "play") {
+    let interval: NodeJS.Timeout;
+    if (seekBar.current) seekBar.current.style.width = "0%";
+    if (currentSongAction === "play") {
       interval = setInterval(() => {
-        if (seekBar.current && song.duration) {
+        if (seekBar.current) {
           const percentage = changeColorOfTheSeeker();
-          console.log(percentage)
+          if (percentage! >= 100) {
+            setCurrentSongAction("pause");
+            clearInterval(interval);
+          }
           seekBar.current.style.width = `${percentage}%`;
         }
       }, 1000);
     }
 
-    return () => clearInterval(interval)
-  }, [song, currentSongAction]);
+    return () => clearInterval(interval);
+  }, [song, currentSongAction, songIndex]);
 
   useEffect(() => {
-    if(songRef.current) {
-      if(currentSongAction === "pause")
-        songRef.current.pause()
-      else
-        songRef.current.play()
+    if (songRef.current) {
+      if (currentSongAction === "pause") songRef.current.pause();
+      else songRef.current.play();
     }
-  }, [currentSongAction])
-
+  }, [currentSongAction]);
 
   return (
     <>
@@ -70,15 +84,16 @@ const CurrentlyPlayingSongDisplay = () => {
 
           {/* for seeker */}
           <div className="w-96 mt-6">
-            {/* <input
-              type="range"
-              min="0"
-              max={song.duration}
-              // value={}
-              className="w-full mt-2"
-            /> */}
-            <audio ref={songRef} src={song.url} autoPlay />
-            <div className="bg-gray-600 rounded-full cursor-pointer">
+            <audio
+              ref={songRef}
+              src={song.url}
+              autoPlay={currentSongAction === "play" ? true : false}
+            />
+            <div
+              ref={divRef}
+              className="bg-gray-600 rounded-full cursor-pointer"
+              onClick={handleSeekBarClick}
+            >
               <hr
                 ref={seekBar}
                 className={`border-none bg-white p-0.5 rounded-full`}
@@ -92,11 +107,35 @@ const CurrentlyPlayingSongDisplay = () => {
             </div>
 
             <div className="flex items-center gap-x-3">
-              <img onClick={() => dispatch(passSongIndexToStore({ songIndex: songIndex - 1 }))} src={backward} alt="backward" className="cursor-pointer" />
-              <div onClick={() => setCurrentSongAction(prev => prev === "play" ? "pause" : "play")} className="border rounded-full w-10 h-10 bg-white flex justify-center items-center cursor-pointer">
-                <img src={currentSongAction === "play" ? pause : play} alt="options" />
+              <img
+                onClick={() =>
+                  dispatch(passSongIndexToStore({ songIndex: songIndex - 1 }))
+                }
+                src={backward}
+                alt="backward"
+                className="cursor-pointer"
+              />
+              <div
+                onClick={() =>
+                  setCurrentSongAction((prev) =>
+                    prev === "play" ? "pause" : "play"
+                  )
+                }
+                className="border rounded-full w-10 h-10 bg-white flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={currentSongAction === "play" ? pause : play}
+                  alt="options"
+                />
               </div>
-              <img onClick={() => dispatch(passSongIndexToStore({ songIndex: songIndex + 1 }))} src={forward} alt="forward" className="cursor-pointer" />
+              <img
+                onClick={() =>
+                  dispatch(passSongIndexToStore({ songIndex: songIndex + 1 }))
+                }
+                src={forward}
+                alt="forward"
+                className="cursor-pointer"
+              />
             </div>
 
             <div className="border rounded-full w-10 h-10 bg-[#FFFFFF1A] flex justify-center items-center cursor-pointer">
